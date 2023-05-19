634897de9b4ea6c0ab0bab94326aebc127af4c4d
Mohamed <s2000588@edu.bc.fi>
2023-05-20 01:47:53 +0300

    update booking controllers

--- controllers/BookingControllers.js
+++ controllers/BookingControllers.js
@@ -1,4 +1,5 @@
 import BookingModel from "../models/Booking.js";
+import CarModel from "../models/Car.js";
 
 // @desc    Book a car
 // @route   POST /api/v1/bookings
@@ -9,6 +10,16 @@ const bookCar = async (req, res) => {
 
     const booking = await BookingModel.create(req.body);
 
+    // Update the car bookedTimeSlots array with the new booking time slot Object
+    const car = await CarModel.findOne({ _id: req.body.car });
+    // car.bookedTimeSlots.push({
+    //   from: req.body.bookedTimeSlot.from,
+    //   to: req.body.bookedTimeSlot.to,
+    // });
+    car.bookedTimeSlots.push(req.body.bookedTimeSlot);
+    
+    // save the car
+    await car.save();
     res.status(201).json({
       success: true,
       data: booking,
