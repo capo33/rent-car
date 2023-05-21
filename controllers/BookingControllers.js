24ed0a75044909bc2a89971f510903040f23c69c
Mohamed <s2000588@edu.bc.fi>
2023-05-22 00:56:59 +0300

    add strip payment

--- controllers/BookingControllers.js
+++ controllers/BookingControllers.js
@@ -1,29 +1,65 @@
+import dotenv from "dotenv";
 import BookingModel from "../models/Booking.js";
+
 import CarModel from "../models/Car.js";
+import Stripe from "stripe";
+
+dotenv.config();
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+  apiVersion: "  2022-11-15",
+});
 
 // @desc    Book a car
 // @route   POST /api/v1/bookings
 // @access  Private
+
 const bookCar = async (req, res) => {
+  const { token } = req.body;
+  console.log("req.body", req.body);
+  console.log("token", token);
   try {
-    req.body.transactionId = "1234";
-
-    const booking = await BookingModel.create(req.body);
-
-    // Update the car bookedTimeSlots array with the new booking time slot Object
-    const car = await CarModel.findOne({ _id: req.body.car });
-    // car.bookedTimeSlots.push({
-    //   from: req.body.bookedTimeSlot.from,
-    //   to: req.body.bookedTimeSlot.to,
-    // });
-    car.bookedTimeSlots.push(req.body.bookedTimeSlot);
-    
-    // save the car
-    await car.save();
-    res.status(201).json({
-      success: true,
-      data: booking,
+    const customer = await stripe.customers.create({
+      email: token.email,
+      source: token.id,
     });
+
+    const payment = await stripe.charges.create(
+      {
+        amount: req.body.totalAmount * 100,
+        currency: "eur",
+        customer: customer.id,
+        receipt_email: token.email,
+      },
+      {
+        idempotencyKey: Math.random().toString(16),
+      }
+    );
+
+    if (payment) {
+      req.body.transactionId = payment.source.id;
+
+      const booking = await BookingModel.create(req.body);
+
+      // Update the car bookedTimeSlots array with the new booking time slot Object
+      const car = await CarModel.findOne({ _id: req.body.car });
+      // car.bookedTimeSlots.push({
+      //   from: req.body.bookedTimeSlot.from,
+      //   to: req.body.bookedTimeSlot.to,
+      // });
+      car.bookedTimeSlots.push(req.body.bookedTimeSlot);
+
+      // save the car
+      await car.save();
+      res.status(201).json({
+        success: true,
+        data: booking,
+      });
+    } else {
+      res.status(400).json({
+        success: false,
+        message: "Payment failed",
+      });
+    }
   } catch (error) {
     res.status(400).json({
       success: false,
