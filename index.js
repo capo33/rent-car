cdc75aa3cdc59c7baf35c7a4964d77d9eeb4f610
Mohamed <s2000588@edu.bc.fi>
2023-05-23 14:40:42 +0300

    delete unnecessary files

--- index.js
+++ index.js
@@ -30,6 +30,15 @@ app.use("/api/v1/auth", userRoutes);
 app.use("/api/v1/cars", carRoutes);
 app.use("/api/v1/bookings", bookingRoutes);
 
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("frontend/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+}
 
 // Start server
 
